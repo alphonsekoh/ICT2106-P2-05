c33e45ab7f6ee876b0880114dea9468b90f98fe9
ackillben1 <angyimin98@gmail.com>
2022-02-15 18:29:54 +0800

    Added dept controller and views from scaffold

---
+++ Areas/Admin/Controllers/DepartmentsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PainAssessment.Areas.Admin.Data;
+using PainAssessment.Areas.Admin.Models;
+using PainAssessment.Areas.Admin.Services;
+
+namespace PainAssess.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly IDepartmentService departmentService;
+
+        public DepartmentsController(IDepartmentService departmentService)
+        {
+            this.departmentService = departmentService;
+        }
+
+        // GET: Departments
+        public IActionResult Index()
+        {
+            return View(departmentService.GetAllDepartments());
+        }
+
+        // GET: Departments/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = departmentService.GetDepartment((int)id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // GET: Departments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Name")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                departmentService.CreateDepartment(department);
+                departmentService.SaveDepartment();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = departmentService.GetDepartment((int)id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Departments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Name")] Department department)
+        {
+            if (id != department.DepartmentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    departmentService.UpdateDepartment(department);
+                    departmentService.SaveDepartment();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (departmentService.GetDepartment(department.DepartmentID) == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var department = departmentService.GetDepartment((int)id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var department = departmentService.GetDepartment((int)id);
+            departmentService.DeleteDepartment(department.DepartmentID);
+            departmentService.SaveDepartment();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
