86f53b0d8774f7734684ea6852ce8e50c24474a3
jrenjq <97725487+jrenjq@users.noreply.github.com>
2022-02-19 13:53:18 +0800

    Update Data

---
+++ Areas/Module2/Data/PainAssessmentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PainAssessment.Areas.ModuleTwo.Models;
+
+namespace PainAssessment.Data
+{
+    public class PainAssessmentContext : DbContext
+    {
+        public PainAssessmentContext (DbContextOptions<PainAssessmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PainAssessment.Areas.ModuleTwo.Models.Checklist> Checklist { get; set; }
+    }
+}
