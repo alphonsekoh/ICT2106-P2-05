516020d29467ef7c0d07901e0669a9ab4c628943
Jaspzx <jaspzx@hotmail.com>
2022-02-28 01:27:17 +0800

    Added new branch for development

---
+++ Areas/ModuleTwo/Controllers/PatientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PainAssessment.Areas.ModuleTwo.Controllers
+{
+    [Area("ModuleTwo")]
+    public class PatientController : Controller
+    {
+        // GET: ../ModuleTwo/Patient
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: ../ModuleTwo/Patient/Details
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/ModuleTwo/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PainAssessment.Areas.ModuleTwo.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string patientName { get; set; }
+        public int patientAge { get; set; }
+        public string condition { get; set; }
+        public string gender { get; set; }
+    }
+}
