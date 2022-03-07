a04410415f072b01f4a31896627cddbf59af3635
xcxpcxkes <2002411@sit.singaporetech.edu.sg>
2022-03-08 01:27:42 +0800

    Added LoginService and Interface

---
+++ Domain/LoginService.cs
@@ -0,0 +1,37 @@
+﻿using Org.BouncyCastle.Crypto.Generators;
+using PainAssessment.Interfaces;
+using PainAssessment.Models;
+using System;
+using System.Collections.Generic;
+using BC = BCrypt.Net.BCrypt;
+
+namespace PainAssessment.Domain
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LoginService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public bool Login(int accountId, string password)
+        {
+
+            Account account = _unitOfWork.AccountRepository.GetById(accountId);
+            // Check if account exists and password matches to the one in db
+            if (account != null || BC.Verify(password, account.Password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+}
---
+++ Interfaces/ILoginService.cs
@@ -0,0 +1,11 @@
+﻿using PainAssessment.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PainAssessment.Interfaces
+{
+    public interface ILoginService
+    {
+        bool Login(int accountId, string password);
+    }
+}
