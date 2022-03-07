0b4b1bf7ea5a505ac88fdeb0f8f2d59f4dffc1ee
Ren Jiaqi <jrenjq@gmail.com>
2022-03-07 11:47:51 +0800

    Factory pattern for Calculate Weightage Service

---
+++ Areas/ModuleTwo/Services/CalculateWeightageService.cs
@@ -0,0 +1,20 @@
+﻿using PainAssessment.Areas.ModuleTwo.Models.CalculateWeightageModels;
+using PainAssessment.Areas.ModuleTwo.Services;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace PainAssessment.Services
+{
+    public class CalculateWeightageService : ICalculateWeightageService
+    {
+        public ReadOnlyDictionary<string, double> calcWeightageFromThreeInputs(double localInput, double centralInput, double regionalInput)
+        {
+            return new WeightageObjectFromThreeDoubles(localInput, centralInput, regionalInput).getThreeWeightagesAsStrDoubleDict();
+        }
+
+        public ReadOnlyDictionary<string, double> calcWeightageFromThreeInputs(string localInput, string centralInput, string regionalInput)
+        {
+            return new WeightageObjectFromThreeStrings(localInput, centralInput, regionalInput).getThreeWeightagesAsStrDoubleDict();
+        }
+    }
+}
---
+++ Areas/ModuleTwo/Services/ICalculateWeightageService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace PainAssessment.Areas.ModuleTwo.Services
+{
+    public interface ICalculateWeightageService
+    {
+        public ReadOnlyDictionary<string, double> calcWeightageFromThreeInputs(double localInput, double centralInput, double regionalInput);
+        public ReadOnlyDictionary<string, double> calcWeightageFromThreeInputs(string localInput, string centralInput, string regionalInput);
+    }
+}
