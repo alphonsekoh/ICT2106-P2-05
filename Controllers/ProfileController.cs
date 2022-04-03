62c1365fa687d48864cd1c58b3d2540b9fa40c8c
AfnanMuhd <afnan.muhd98@gmail.com>
2022-04-04 01:16:56 +0800

    Updated the views and services for the profile

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -20,15 +20,15 @@ namespace PainAssessment.Controllers
     {
         private readonly ILogger<ProfileController> _logger;
         // Include services
-        private readonly IAccountService accountService;
+        private readonly ILoginService loginService;
         private readonly IPractitionerService practitionerService;
         private readonly IAdministratorService administratorService;
         private readonly IClinicalAreaService clinicalAreaService;
 
-        public ProfileController(ILogger<ProfileController> logger, IAccountService accountService, IPractitionerService practitionerService, IAdministratorService administratorService, IClinicalAreaService clinicalAreaService)
+        public ProfileController(ILogger<ProfileController> logger, ILoginService loginService,  IPractitionerService practitionerService, IAdministratorService administratorService, IClinicalAreaService clinicalAreaService)
         {
             _logger = logger;
-            this.accountService = accountService;
+            this.loginService = loginService;
             this.practitionerService = practitionerService;
             this.administratorService = administratorService;
             this.clinicalAreaService = clinicalAreaService;
@@ -37,49 +37,52 @@ namespace PainAssessment.Controllers
 
         public ActionResult ViewProfile()
         {
-            //var userid = User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            var role = User.FindFirst(ClaimTypes.Role).Value;
 
-            String userid = "admin1";
+            var userid = loginService.GetAccountId();
+            var user = loginService.GetAccount(userid);
 
-            Account userAcc = accountService.GetAccount(userid);
-            if (role == "Administrator")
+            switch (user.Role)
             {
-                Administrator admin = administratorService.GetOneAdmin(userAcc.AccountId);
-                ClinicalArea clinical = clinicalAreaService.GetClinicalArea(admin.ClinicalAreaID);
-                var adminViewModel = new AdministratorModel
-                {
-                    Name = admin.Account.Username,
-                    FullName = admin.FullName,
-                    Role = admin.Account.Role,
-                    Experience = admin.Experience,
-                    ClinicalArea = clinical.Name,
-                    AccountID = admin.Account.AccountId
+                case "Administrator":
+                    // admin
+                    Administrator admin = administratorService.GetOneAdmin(userid);
+                    ClinicalArea clinical = clinicalAreaService.GetClinicalArea(admin.ClinicalAreaID);
+                    var adminViewModel = new AdministratorModel
+                    {
+                        Name = admin.Account.Username,
+                        FullName = admin.FullName,
+                        Role = admin.Account.Role,
+                        Experience = admin.Experience,
+                        ClinicalArea = clinical.Name,
+                        AccountID = admin.Account.AccountId
 
-                };
-                string jsonString = JsonSerializer.Serialize(adminViewModel);
-                return RedirectToAction(actionName: "ViewAdmin", controllerName: "Home",
-        new { data= jsonString, area = "Admin" });
-                //return View("ViewAdmin",adminViewModel);
-            }
-            else
-            {
-                Practitioner practionerDetails = practitionerService.GetPractitioner(userAcc.AccountId);
-                ClinicalArea clinical = clinicalAreaService.GetClinicalArea(practionerDetails.ClinicalAreaID);
-                var practionerViewModel = new PractionerModel
-                {
-                    Name = practionerDetails.Name,
-                    FullName = userAcc.Username,
-                    Role = userAcc.Role,
-                    AccountID = userAcc.AccountId,
-                    PriorPainEducation = practionerDetails.PriorPainEducation,
-                    ClinicalArea = clinical.Name,
-                    PracticeType = practionerDetails.PracticeType.Name
+                    };
+                    string jsonString = JsonSerializer.Serialize(adminViewModel);
+                    return RedirectToAction(actionName: "ViewAdmin", controllerName: "Home",
+                        new { data = jsonString, area = "Admin" });
 
-                };
-                return View("ViewPractioner", practionerViewModel);
+                case "Practitioner":
+                    // practitioner
+                    Practitioner practionerDetails = practitionerService.GetPractitioner(new Guid("a6e08001-f5e1-442e-d40f-08da11a4a882"));
+                    ClinicalArea clinicalPrac = clinicalAreaService.GetClinicalArea(practionerDetails.ClinicalAreaID);
+                    var practionerViewModel = new PractionerModel
+                    {
+                        Name = practionerDetails.Name,
+                        FullName = user.Username,
+                        Role = user.Role,
+                        AccountID = user.AccountId,
+                        PriorPainEducation = practionerDetails.PriorPainEducation,
+                        ClinicalArea = clinicalPrac.Name,
+                        PracticeType = practionerDetails.PracticeType.Name
+
+                    };
+                    return View("ViewPrac", practionerViewModel);
+                default:
+                    // unrecognised method; return to the blank form
+                    return RedirectToAction("Index");
+
             }
-            
+
         }
 
 
