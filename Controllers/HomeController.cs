16e0858414530efb9728431235fd44ac0544ba28
Abcnian <phssniankiat@hotmail.com>
2022-04-06 20:49:25 +0800

    update view template checklist
    add create account tabs in all 3 layout
    remove unnecessary functions in homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,7 +26,6 @@ namespace PainAssessment.Controllers
 
         public IActionResult Index()
         {
-            //var templateChecklistArr = templateChecklistService.GetAllTemplateChecklist().ToList();
             return View();
         }
 
@@ -34,65 +33,6 @@ namespace PainAssessment.Controllers
         {
             return View();
         }
-
-        public IActionResult ViewTemplateChecklist()
-        {
-            return View();
-        }
-
-        /*public IActionResult ManageTemplateChecklist(int num)
-        {
-            var templateQuestionsArr = defaultQuestionsService.GetAllDefaultQuestionsFromTemplateChecklist(num).ToList();
-            return View(templateQuestionsArr);
-        }*/
-       /* public IActionResult ManageTemplateChecklist(int checklistID)
-        {
-            var templateQuestionsArr = defaultQuestionsService.GetAllDefaultQuestionsFromTemplateChecklist(checklistID).ToList();
-            return View(templateQuestionsArr);
-        }*/
-
-
-        /*public IActionResult ViewProfile()
-        {
-            return View();
-        }*/
-
-        [HttpPost]
-        public string UpdateQuestion(int DQID, string QString, string weightage)
-        {
-            Debug.Write(weightage);
-            DefaultQuestion temp = defaultQuestionsService.GetDefaultQuestion(DQID);
-
-            defaultQuestionsService.UpdateDefaultQuestion(DQID, QString, temp.QDescription, temp.PainSection, double.Parse(weightage));
-
-
-
-            return "Privacy";
-        }
-
-        [HttpPost]
-        public string AddQuestion(int DQID, string QString, string PainSection, string weightage)
-        {
-
-            defaultQuestionsService.CreateDefaultQuestion(1, QString, "", int.Parse(PainSection), double.Parse(weightage));
-
-            return "nth wrong";
-        }
-
-        [HttpPost]
-        public string DeleteQuestion(int DQID)
-        {
-            Console.WriteLine(DQID);
-            defaultQuestionsService.DeleteDefaultQuestion(DQID);
-            return "nth wrong";
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
 
         public IActionResult ErrorPage(int? statusCode = null)
         {
